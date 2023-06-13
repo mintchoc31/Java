2ee67aa7f68c35c4ed2118af1d6c6e15d5e7aa45
mintchoc31 <mintchoc31@gmail.com>
2023-06-13 16:45:00 +0900

    Java 이클립스 연습

---
+++ TextBook/src/ch01/sec06/Hello.java
@@ -0,0 +1,14 @@
+package ch01.sec06;
+
+/*
+ * 날짜:2023/06/12
+ * 이름:조유빈
+ * 내용:
+ * 
+ */
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, Java");
+	}
+}
---
+++ TextBook/src/ch01/sec11/Hello.java
@@ -0,0 +1,16 @@
+package ch01.sec11;
+
+/*
+ * 날짜:2023/06/12
+ * 이름:조유빈
+ * 내용:
+ * 
+ */
+
+public class Hello {
+	//프로그램 실행 진입점
+	public static void main(String[] args) {
+      //콘솔에 출력하는 실행문
+		System.out.println("Hello, Java");
+	}
+}
