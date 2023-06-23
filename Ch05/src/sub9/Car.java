b2ee3359b7448875a3939b4f4649178d3e87d9e8
mintchoc31 <mintchoc31@gmail.com>
2023-06-23 17:56:21 +0900

    Ch05 파일 업데이트

---
+++ Ch05/src/sub9/Car.java
@@ -0,0 +1,10 @@
+package sub9;
+
+public interface Car {
+	
+	public abstract void speedUp(int speed);
+	public abstract void speedDown(int speed);
+	public void show();   // abstract 생략 가능 (인터페이스에서)
+	
+	
+}
